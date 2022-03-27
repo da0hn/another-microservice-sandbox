de9f91385da76cfebe6cf6c795bd3241070ce682
da0hn <gabrielhondacba@gmail.com>
2022-03-27 13:20:30 -0400

    feat: add initial `express` configuration

---
+++ auth-api/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const env = process.env;
+const PORT = env.PORT || 8080;
+
+app.get('/api/status', (request, response) => {
+  return response.json({
+    service: 'auth-api',
+    status: 'up',
+    httpStatus: 200
+  })
+})
+
+app.listen(PORT, () => {
+  console.info(`Server started successfully at port ${PORT}`)
+})
+
