c84c5214574f3da40dd57e9a4b0e0bb72b58e134
da0hn <gabrielhondacba@gmail.com>
2022-03-28 15:32:10 -0400

    feat: create microservice `product`

---
+++ product-api/src/main/java/br/com/gabriel/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProductApplication.class, args);
+  }
+
+}
