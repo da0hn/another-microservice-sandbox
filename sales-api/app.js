6f5ddef4473750cc0e789f5f0f9c1cba9b4a611a
da0hn <gabrielhondacba@gmail.com>
2022-03-29 09:45:09 -0400

    feat: add `status` endpoint

--- sales-api/app.js
+++ sales-api/app.js
@@ -5,6 +5,15 @@ const app = express();
 const env = process.env;
 const PORT = env.PORT || 8002;
 
+app.get('/api/sales/status', (request, response) => {
+  return response.status(200).json({
+    service: 'sales-api',
+    status: 'up',
+    httpStatus: 200
+  })
+})
+
+
 app.listen(PORT, () => {
   console.info(`Server started successfully at port ${PORT}`);
 });
