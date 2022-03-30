f5894c22a425bd1e5176ef49f2213ddaec8de4ea
da0hn <gabrielhondacba@gmail.com>
2022-03-29 21:17:25 -0400

    refactor: change default port to `8082`

--- auth-api/app.js
+++ auth-api/app.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 const env = process.env;
-const PORT = env.PORT || 8001;
+const PORT = env.PORT || 8082;
 
 app.get('/api/authentication/status', (request, response) => {
   return response.json({
