c6589e5bc0c582dba4bd681775aac63274305c70
da0hn <gabrielhondacba@gmail.com>
2022-03-29 21:17:16 -0400

    refactor: change default port to `8081`

--- sales-api/app.js
+++ sales-api/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 const env = process.env;
-const PORT = env.PORT || 8002;
+const PORT = env.PORT || 8081;
 
 app.get('/api/sales/status', (request, response) => {
   return response.status(200).json({
