5cc62da7ccd836575812a3b30dbb9503104281ab
da0hn <gabrielhondacba@gmail.com>
2022-03-30 10:20:21 -0400

    feat: create initial data on startup

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,18 +1,22 @@
-import express from "express";
+import express from 'express';
+import * as db from './src/config/db/initialData.js';
+
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
 
+await db.createInitialData();
+
 app.get('/api/authentication/status', (request, response) => {
   return response.json({
     service: 'auth-api',
     status: 'up',
-    httpStatus: 200
+    httpStatus: 200,
   })
 })
 
 app.listen(PORT, () => {
-  console.info(`Server started successfully at port ${PORT}`)
+  console.info(`Server started successfully at port ${ PORT }`)
 })
 
---
+++ auth-api/src/config/db/initialData.js
@@ -0,0 +1,20 @@
+import User from '../../modules/user/model/user.js';
+import bcrypt from 'bcrypt';
+
+
+export async function createInitialData() {
+  try {
+    await User.sync({force: true});
+
+    const password = await bcrypt.hash('123456', 10);
+
+    await User.create({
+      name: 'user test',
+      email: 'testuser@gmail.com',
+      password,
+    });
+  } catch (e) {
+    console.error(`A error occurs: ${ e.message }`)
+  }
+}
+
