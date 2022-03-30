d26bf1c230aba4cf7ed11c987cca55a7316a1932
da0hn <gabrielhondacba@gmail.com>
2022-03-30 10:20:01 -0400

    feat: add `user` table model

---
+++ auth-api/src/modules/user/model/user.js
@@ -0,0 +1,27 @@
+import sequelize from '../../../config/db/dbConfig.js';
+import { Sequelize } from 'sequelize';
+
+
+const User = sequelize.define('user', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    },
+    {},
+);
+
+export default User;
