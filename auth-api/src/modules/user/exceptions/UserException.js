00cb12f1ef937e546daeb592799760306cd7e3e1
da0hn <gabrielhondacba@gmail.com>
2022-04-16 09:46:35 -0400

    feat: add customized user exception

---
+++ auth-api/src/modules/user/exceptions/UserException.js
@@ -0,0 +1,10 @@
+class UserException extends Error {
+  constructor(status, message) {
+    super(message);
+    this.status = status;
+    this.message = message;
+    this.name = this.constructor.name
+    Error.captureStackTrace(this.this.constructor)
+  }
+}
+
