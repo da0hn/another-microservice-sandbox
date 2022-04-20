7fa9bdb738ed6646505ba61d0a6da02d59b8d16c
da0hn <gabrielhondacba@gmail.com>
2022-04-20 10:16:06 -0400

    chore: add `spring-data-jpa` configuration

--- product-api/src/main/java/br/com/gabriel/product/ProductApplication.java
+++ product-api/src/main/java/br/com/gabriel/product/ProductApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductApplication {
 
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     SpringApplication.run(ProductApplication.class, args);
   }
 
