944b9fc44b7834ff3ebc49a9553df130f00773bf
da0hn <gabrielhondacba@gmail.com>
2022-04-20 08:48:24 -0400

    feat: add more users

--- auth-api/src/config/db/initialData.js
+++ auth-api/src/config/db/initialData.js
@@ -4,17 +4,30 @@ import bcrypt from 'bcrypt';
 
 export async function createInitialData() {
   try {
-    await User.sync({force: true});
+    await User.sync({ force: true });
 
     const password = await bcrypt.hash('123456', 10);
 
     await User.create({
-      name: 'user test',
-      email: 'testuser@gmail.com',
+      name: 'user test 1',
+      email: 'testuser1@gmail.com',
       password,
     });
-  } catch (e) {
-    console.error(`A error occurs: ${ e.message }`)
+
+    await User.create({
+      name: 'user test 2',
+      email: 'testuser2@gmail.com',
+      password,
+    });
+
+    await User.create({
+      name: 'user test 3',
+      email: 'testuser3@gmail.com',
+      password,
+    });
+
+  } catch (error) {
+    console.error(`A error occurs: ${ error.message }`);
   }
 }
 
