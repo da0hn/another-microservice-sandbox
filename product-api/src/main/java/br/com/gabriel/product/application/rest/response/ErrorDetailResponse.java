63c004c47cb98ebf93d7ea3d1439036c46831506
da0hn <gabrielhondacba@gmail.com>
2022-04-25 13:27:11 -0400

    feat: add error details response

---
+++ product-api/src/main/java/br/com/gabriel/product/application/rest/response/ErrorDetailResponse.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.product.application.rest.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public record ErrorDetailResponse(
+  int status,
+  String message,
+  @JsonFormat(pattern = "dd-MM-yyyy hh:mm:ss")
+  LocalDateTime instant
+) {
+}
