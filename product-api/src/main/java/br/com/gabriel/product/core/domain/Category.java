4139722e665a2ea02f947391a18526f20150f333
da0hn <gabrielhondacba@gmail.com>
2022-04-26 18:42:45 -0400

    feat: add validation of `Category` state

--- product-api/src/main/java/br/com/gabriel/product/core/domain/Category.java
+++ product-api/src/main/java/br/com/gabriel/product/core/domain/Category.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.util.ObjectUtils;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,11 +14,11 @@ import java.io.Serial;
 import java.util.List;
 
 @Entity
-@Table(name = "category")
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "category")
 public class Category extends BaseEntity {
 
   @Serial private static final long serialVersionUID = 463544359525272305L;
@@ -28,5 +29,9 @@ public class Category extends BaseEntity {
   @OneToMany(mappedBy = "category")
   private List<Product> products;
 
-
+  public void validate() {
+    if(ObjectUtils.isEmpty(this.description)) {
+      throw new ValidationException("The category description was not informed");
+    }
+  }
 }
