7b531afce8cec1d46360af157b9449dba7853632
da0hn <gabrielhondacba@gmail.com>
2022-04-26 18:26:05 -0400

    feat: add empty output representation

---
+++ product-api/src/main/java/br/com/gabriel/product/application/rest/response/EmptyResponse.java
@@ -0,0 +1,6 @@
+package br.com.gabriel.product.application.rest.response;
+
+import br.com.gabriel.product.core.services.ExecutableService;
+
+public record EmptyResponse() implements ExecutableService.Output {
+}
