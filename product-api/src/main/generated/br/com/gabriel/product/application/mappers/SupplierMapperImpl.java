c85b65158c2b28d6e73f6855b432dc3da7aec45b
da0hn <gabrielhondacba@gmail.com>
2022-04-26 22:07:37 -0400

    feat: generate implementation of `SupplierMapper`

---
+++ product-api/src/main/generated/br/com/gabriel/product/application/mappers/SupplierMapperImpl.java
@@ -0,0 +1,46 @@
+package br.com.gabriel.product.application.mappers;
+
+import br.com.gabriel.product.application.rest.request.CreateSupplierRequest;
+import br.com.gabriel.product.application.rest.response.SupplierResponse;
+import br.com.gabriel.product.core.domain.Supplier;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-04-26T22:06:30-0400",
+    comments = "version: 1.5.0.RC1, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+)
+@Component
+public class SupplierMapperImpl implements SupplierMapper {
+
+    @Override
+    public SupplierResponse fromEntity(Supplier entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String name = null;
+
+        id = entity.getId();
+        name = entity.getName();
+
+        SupplierResponse supplierResponse = new SupplierResponse( id, name );
+
+        return supplierResponse;
+    }
+
+    @Override
+    public Supplier toEntity(CreateSupplierRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        Supplier supplier = new Supplier();
+
+        supplier.setName( request.name() );
+
+        return supplier;
+    }
+}
