cbb5bae64a76f41c291b29de7a9064282aa3b67d
da0hn <gabrielhondacba@gmail.com>
2022-04-27 17:46:33 -0400

    fix: remove `id` attribute from request

--- product-api/src/main/java/br/com/gabriel/product/application/rest/request/CreateProductRequest.java
+++ product-api/src/main/java/br/com/gabriel/product/application/rest/request/CreateProductRequest.java
@@ -3,7 +3,6 @@ package br.com.gabriel.product.application.rest.request;
 import br.com.gabriel.product.core.services.ExecutableService;
 
 public record CreateProductRequest(
-  Long id,
   String name,
   Integer quantityAvailable,
   Long categoryId,
