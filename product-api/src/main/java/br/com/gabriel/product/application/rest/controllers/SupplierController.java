d154f577c0f72d90ba89f0e828d1250cbe4eb43d
da0hn <gabrielhondacba@gmail.com>
2022-04-30 15:39:24 -0400

    feat: add endpoint to delete supplier by id

--- product-api/src/main/java/br/com/gabriel/product/application/rest/controllers/SupplierController.java
+++ product-api/src/main/java/br/com/gabriel/product/application/rest/controllers/SupplierController.java
@@ -1,10 +1,14 @@
 package br.com.gabriel.product.application.rest.controllers;
 
 import br.com.gabriel.product.application.rest.request.CreateSupplierRequest;
+import br.com.gabriel.product.application.rest.request.EntityIdRequest;
 import br.com.gabriel.product.application.rest.response.SupplierResponse;
 import br.com.gabriel.product.core.services.CreateSupplierService;
+import br.com.gabriel.product.core.services.DeleteSupplierByIdService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class SupplierController {
 
   private final CreateSupplierService createSupplierService;
+  private final DeleteSupplierByIdService deleteSupplierByIdService;
 
   @PostMapping
   public ResponseEntity<SupplierResponse> create(@RequestBody final CreateSupplierRequest request) {
     return ResponseEntity.ok(this.createSupplierService.execute(request));
+  }
+
+  @DeleteMapping("/{productId}")
+  public ResponseEntity<Void> deleteById(@PathVariable final Long productId) {
+    this.deleteSupplierByIdService.execute(new EntityIdRequest(productId));
+    return ResponseEntity.noContent().build();
   }
 
 }
---
+++ product-api/src/main/java/br/com/gabriel/product/core/services/DeleteSupplierByIdService.java
@@ -0,0 +1,29 @@
+package br.com.gabriel.product.core.services;
+
+import br.com.gabriel.product.application.rest.request.EntityIdRequest;
+import br.com.gabriel.product.application.rest.response.EmptyResponse;
+import br.com.gabriel.product.core.domain.ValidationException;
+import br.com.gabriel.product.infra.db.repositories.JpaSupplierRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+@AllArgsConstructor
+public class DeleteSupplierByIdService implements ExecutableService<EntityIdRequest, EmptyResponse> {
+
+  private final JpaSupplierRepository repository;
+
+
+  @Override public EmptyResponse execute(final EntityIdRequest input) {
+
+    if(Objects.isNull(input.id())) {
+      throw new ValidationException("The id must be informed");
+    }
+
+    this.repository.deleteById(input.id());
+
+    return EmptyResponse.empty();
+  }
+}
