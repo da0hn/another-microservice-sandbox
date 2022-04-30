5a0dc54b19f5472c12dbd588c1eef45fecc185b2
da0hn <gabrielhondacba@gmail.com>
2022-04-30 15:38:27 -0400

    feat: add endpoint to delete product by id

--- product-api/src/main/java/br/com/gabriel/product/application/rest/controllers/ProductController.java
+++ product-api/src/main/java/br/com/gabriel/product/application/rest/controllers/ProductController.java
@@ -1,10 +1,14 @@
 package br.com.gabriel.product.application.rest.controllers;
 
 import br.com.gabriel.product.application.rest.request.CreateProductRequest;
+import br.com.gabriel.product.application.rest.request.EntityIdRequest;
 import br.com.gabriel.product.application.rest.response.ProductResponse;
 import br.com.gabriel.product.core.services.CreateProductService;
+import br.com.gabriel.product.core.services.DeleteProductByIdService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
   private final CreateProductService createProductService;
+  private final DeleteProductByIdService deleteProductByIdService;
 
   @PostMapping
   public ResponseEntity<ProductResponse> create(@RequestBody final CreateProductRequest request) {
     return ResponseEntity.ok(this.createProductService.execute(request));
+  }
+
+
+  @DeleteMapping("/{productId}")
+  public ResponseEntity<Void> deleteById(@PathVariable final Long productId) {
+    this.deleteProductByIdService.execute(new EntityIdRequest(productId));
+    return ResponseEntity.noContent().build();
   }
 
 }
