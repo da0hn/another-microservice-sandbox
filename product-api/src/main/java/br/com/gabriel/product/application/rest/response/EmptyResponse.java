3dfa80545730d74c6ee01349f764963823db8440
da0hn <gabrielhondacba@gmail.com>
2022-04-30 15:31:43 -0400

    feat: add empty instance method return

--- product-api/src/main/java/br/com/gabriel/product/application/rest/response/EmptyResponse.java
+++ product-api/src/main/java/br/com/gabriel/product/application/rest/response/EmptyResponse.java
@@ -3,4 +3,9 @@ package br.com.gabriel.product.application.rest.response;
 import br.com.gabriel.product.core.services.ExecutableService;
 
 public record EmptyResponse() implements ExecutableService.Output {
+
+  public static EmptyResponse empty() {
+    return new EmptyResponse();
+  }
+
 }
