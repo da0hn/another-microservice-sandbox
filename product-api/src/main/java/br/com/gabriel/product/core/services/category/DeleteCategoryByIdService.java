11dc3678765c81f3f37271a0a95c0c1652780369
da0hn <gabrielhondacba@gmail.com>
2022-04-30 16:49:42 -0400

    fix: validate category relation with another product

--- product-api/src/main/java/br/com/gabriel/product/core/services/category/DeleteCategoryByIdService.java
+++ product-api/src/main/java/br/com/gabriel/product/core/services/category/DeleteCategoryByIdService.java
@@ -5,6 +5,7 @@ import br.com.gabriel.product.application.rest.response.EmptyResponse;
 import br.com.gabriel.product.core.domain.ValidationException;
 import br.com.gabriel.product.core.services.ExecutableService;
 import br.com.gabriel.product.infra.db.repositories.JpaCategoryRepository;
+import br.com.gabriel.product.infra.db.repositories.JpaProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,15 +16,21 @@ import java.util.Objects;
 public class DeleteCategoryByIdService implements ExecutableService<EntityIdRequest, EmptyResponse> {
 
   private final JpaCategoryRepository repository;
+  private final JpaProductRepository productRepository;
 
 
   @Override public EmptyResponse execute(final EntityIdRequest input) {
+    final var categoryId = input.id();
 
-    if(Objects.isNull(input.id())) {
+    if(Objects.isNull(categoryId)) {
       throw new ValidationException("The id must be informed");
     }
 
-    this.repository.deleteById(input.id());
+    if(this.productRepository.existsByCategoryId(categoryId)) {
+      throw new ValidationException("You cannot delete this category because it's already defined by a product.");
+    }
+
+    this.repository.deleteById(categoryId);
 
     return EmptyResponse.empty();
   }
--- product-api/src/main/java/br/com/gabriel/product/infra/db/repositories/JpaProductRepository.java
+++ product-api/src/main/java/br/com/gabriel/product/infra/db/repositories/JpaProductRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaProductRepository extends JpaRepository<Product, Long> {
+  boolean existsByCategoryId(Long id);
 }
