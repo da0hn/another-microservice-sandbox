2263a0c00a50fc71c7e9fd8edf340164d7f8c143
da0hn <gabrielhondacba@gmail.com>
2022-04-30 14:20:37 -0400

    feat: query categories by description

--- product-api/src/main/java/br/com/gabriel/product/infra/db/repositories/JpaCategoryRepository.java
+++ product-api/src/main/java/br/com/gabriel/product/infra/db/repositories/JpaCategoryRepository.java
@@ -2,9 +2,15 @@ package br.com.gabriel.product.infra.db.repositories;
 
 import br.com.gabriel.product.core.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface JpaCategoryRepository extends JpaRepository<Category, Long> {
+  @Query("select category from Category category where category.description like concat('%', :description, '%')")
+  Collection<Category> findByDescription(@Param("description") String description);
 }
 
