071794cbd09984b4ad0d17dd0ad0e2c87c07840a
da0hn <gabrielhondacba@gmail.com>
2022-05-04 13:05:43 -0400

    feat: add new mapper from entity and sales response

--- product-api/src/main/generated/br/com/gabriel/product/application/mappers/ProductMapperImpl.java
+++ product-api/src/main/generated/br/com/gabriel/product/application/mappers/ProductMapperImpl.java
@@ -3,17 +3,21 @@ package br.com.gabriel.product.application.mappers;
 import br.com.gabriel.product.application.rest.request.CreateProductRequest;
 import br.com.gabriel.product.application.rest.response.CategoryResponse;
 import br.com.gabriel.product.application.rest.response.ProductResponse;
+import br.com.gabriel.product.application.rest.response.ProductSalesResponse;
 import br.com.gabriel.product.application.rest.response.SupplierResponse;
 import br.com.gabriel.product.core.domain.Category;
 import br.com.gabriel.product.core.domain.Product;
 import br.com.gabriel.product.core.domain.Supplier;
+import br.com.gabriel.product.infra.http.SalesByProductResponse;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-05-03T22:49:00-0400",
+    date = "2022-05-04T12:56:31-0400",
     comments = "version: 1.5.0.RC1, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
@@ -42,6 +46,39 @@ public class ProductMapperImpl implements ProductMapper {
         ProductResponse productResponse = new ProductResponse( id, name, quantityAvailable, createdAt, category, supplier );
 
         return productResponse;
+    }
+
+    @Override
+    public ProductSalesResponse fromEntity(Product entity, SalesByProductResponse salesByProduct) {
+        if ( entity == null && salesByProduct == null ) {
+            return null;
+        }
+
+        CategoryResponse category = null;
+        SupplierResponse supplier = null;
+        Long id = null;
+        String name = null;
+        Integer quantityAvailable = null;
+        LocalDateTime createdAt = null;
+        if ( entity != null ) {
+            category = categoryToCategoryResponse1( entity.getCategory() );
+            supplier = supplierToSupplierResponse1( entity.getSupplier() );
+            id = entity.getId();
+            name = entity.getName();
+            quantityAvailable = entity.getQuantityAvailable();
+            createdAt = entity.getCreatedAt();
+        }
+        List<String> sales = null;
+        if ( salesByProduct != null ) {
+            List<String> list = salesByProduct.salesId();
+            if ( list != null ) {
+                sales = new ArrayList<String>( list );
+            }
+        }
+
+        ProductSalesResponse productSalesResponse = new ProductSalesResponse( id, name, quantityAvailable, createdAt, category, supplier, sales );
+
+        return productSalesResponse;
     }
 
     @Override
@@ -75,6 +112,38 @@ public class ProductMapperImpl implements ProductMapper {
     }
 
     protected SupplierResponse supplierToSupplierResponse(Supplier supplier) {
+        if ( supplier == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String name = null;
+
+        id = supplier.getId();
+        name = supplier.getName();
+
+        SupplierResponse supplierResponse = new SupplierResponse( id, name );
+
+        return supplierResponse;
+    }
+
+    protected CategoryResponse categoryToCategoryResponse1(Category category) {
+        if ( category == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String description = null;
+
+        id = category.getId();
+        description = category.getDescription();
+
+        CategoryResponse categoryResponse = new CategoryResponse( id, description );
+
+        return categoryResponse;
+    }
+
+    protected SupplierResponse supplierToSupplierResponse1(Supplier supplier) {
         if ( supplier == null ) {
             return null;
         }
--- product-api/src/main/java/br/com/gabriel/product/application/mappers/ProductMapper.java
+++ product-api/src/main/java/br/com/gabriel/product/application/mappers/ProductMapper.java
@@ -2,7 +2,9 @@ package br.com.gabriel.product.application.mappers;
 
 import br.com.gabriel.product.application.rest.request.CreateProductRequest;
 import br.com.gabriel.product.application.rest.response.ProductResponse;
+import br.com.gabriel.product.application.rest.response.ProductSalesResponse;
 import br.com.gabriel.product.core.domain.Product;
+import br.com.gabriel.product.infra.http.SalesByProductResponse;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -27,6 +29,19 @@ public interface ProductMapper {
     @Mapping(source = "supplier.name", target = "supplier.name")
   })
   ProductResponse fromEntity(Product entity);
+
+  @Mappings({
+    @Mapping(source = "entity.id", target = "id"),
+    @Mapping(source = "entity.name", target = "name"),
+    @Mapping(source = "entity.quantityAvailable", target = "quantityAvailable"),
+    @Mapping(source = "entity.createdAt", target = "createdAt"),
+    @Mapping(source = "entity.category.id", target = "category.id"),
+    @Mapping(source = "entity.category.description", target = "category.description"),
+    @Mapping(source = "entity.supplier.id", target = "supplier.id"),
+    @Mapping(source = "entity.supplier.name", target = "supplier.name"),
+    @Mapping(source = "salesByProduct.salesId", target = "sales")
+  })
+  ProductSalesResponse fromEntity(Product entity, SalesByProductResponse salesByProduct);
 
 
   @Mappings({
