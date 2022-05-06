f84b3a1c3b477d1c79aed2e982e24e39c7bfc682
da0hn <gabrielhondacba@gmail.com>
2022-05-05 21:46:27 -0400

    fix: add `authSource` property in connection url

--- sales-api/src/config/secrets/constants.ts
+++ sales-api/src/config/secrets/constants.ts
@@ -2,7 +2,7 @@ const env = process.env;
 
 export const constants = {
   BASE_URL: '/api/sales-service',
-  MONGO_DB_URL: env.MONGO_DB_URL ?? 'mongodb://localhost:27017/sales-db',
+  MONGO_DB_URL: env.MONGO_DB_URL ?? 'mongodb://root:root@localhost:27017/sales-db?authSource=admin',
 };
 
 
