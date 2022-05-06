5a6517ce9b197f149115c66a47135093c08983e4
da0hn <gabrielhondacba@gmail.com>
2022-05-05 21:54:16 -0400

    feat: add database seeding on server startup

---
+++ sales-api/src/config/db/initialData.ts
@@ -0,0 +1,61 @@
+import OrderSchema from '../../modules/sales/repositories/OrderSchema';
+import { v4 as uuid } from 'uuid';
+import { Status } from '../../modules/sales/model/Order';
+
+export async function createInitialData() {
+  await OrderSchema.collection.drop();
+  await OrderSchema.create({
+    products: [
+      {
+        productId: 1,
+        quantity: 3,
+      },
+      {
+        productId: 2,
+        quantity: 1,
+      },
+      {
+        productId: 3,
+        quantity: 2,
+      },
+    ],
+    user: {
+      id: uuid().toString(),
+      name: 'user test 1',
+      email: 'usertest1@gmail.com',
+    },
+    status: Status.APPROVED,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  await OrderSchema.create({
+    products: [
+      {
+        productId: 1,
+        quantity: 9,
+      },
+      {
+        productId: 2,
+        quantity: 10,
+      },
+    ],
+    user: {
+      id: uuid().toString(),
+      name: 'user test 2',
+      email: 'usertest2@gmail.com',
+    },
+    status: Status.REJECTED,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+}
+
+
+
+
+
+
+
+
+
+
--- sales-api/src/config/db/mongoDbConfig.ts
+++ sales-api/src/config/db/mongoDbConfig.ts
@@ -1,10 +1,13 @@
 import { constants } from '../secrets/constants';
 import { connect, connection } from 'mongoose';
+import { createInitialData } from './initialData';
 
 export function connectInMongoDB() {
   connect(constants.MONGO_DB_URL);
-  connection.on('connected', () => {
+  connection.on('connected', async () => {
     console.info(`The application connected to MongoDB successfully!`);
+    await createInitialData();
+    console.info(`Initial data created successfully`);
   });
   connection.on('error', () => {
     console.error(`Error trying connect on MongoDB`);
