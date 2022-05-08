ce29a1c4b89e4c9ca258a8b132cc96575314a460
da0hn <gabrielhondacba@gmail.com>
2022-05-08 10:29:12 -0400

    feat: add product service url as secret environment variable

--- sales-api/src/config/secrets/secret.ts
+++ sales-api/src/config/secrets/secret.ts
@@ -4,6 +4,7 @@ export const secret = {
   MONGO_DB_URL: env.MONGO_DB_URL ?? 'mongodb://root:root@localhost:27017/sales-db?authSource=admin',
   API_SECRET: env.API_SECRET ?? `YXV0aC1hcGkgc2VjcmV0IGRldiAxMjM0NTY=`,
   RABBITMQ_URL: env.RABBITMQ_URL ?? `amqp://${env.RABBITMQ_USER ?? 'root'}:${env.RABBITMQ_PASS ?? 'root'}@localhost:5672`,
+  PRODUCT_SERVICE: `http://${env.PRODUCT_HOST ?? 'localhost'}:${env.PRODUCT_PORT ?? '8080'}/api/product-service`,
 };
 
 
