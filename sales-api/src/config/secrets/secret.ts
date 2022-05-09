a06208e7093f84bf57c5f8051f748faa920f677b
da0hn <gabrielhondacba@gmail.com>
2022-05-08 22:45:05 -0400

    fix: load correctly `MONGODB_URL` secret

--- sales-api/src/config/secrets/secret.ts
+++ sales-api/src/config/secrets/secret.ts
@@ -1,7 +1,7 @@
 const env = process.env;
 
 export const secret = {
-  MONGO_DB_URL: env.MONGO_DB_URL ?? 'mongodb://root:root@localhost:27017/sales-db?authSource=admin',
+  MONGO_DB_URL: env.MONGODB_URL ?? 'mongodb://root:root@localhost:27017/sales-db?authSource=admin',
   API_SECRET: env.API_SECRET ?? `YXV0aC1hcGkgc2VjcmV0IGRldiAxMjM0NTY=`,
   RABBITMQ_URL: env.RABBITMQ_URL ?? `amqp://${env.RABBITMQ_USER ?? 'root'}:${env.RABBITMQ_PASS ?? 'root'}@${env.RABBITMQ_HOST ?? `localhost`}:${env.RABBITMQ_PORT ?? '5672'}`,
   PRODUCT_SERVICE: `http://${env.PRODUCT_HOST ?? 'localhost'}:${env.PRODUCT_PORT ?? '8080'}/api/product-service`,
